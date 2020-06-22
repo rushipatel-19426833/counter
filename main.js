0953d06eed96fc9144ffe32bc6e6b5ed9aa71f6c
rushipatel-19426833 <56231197+rushipatel-19426833@users.noreply.github.com>
2020-06-23 00:23:54 +0100

    commenting

--- main.js
+++ main.js
@@ -9,19 +9,25 @@ btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
+      // decrease the count number if clicked on decrease button.
       count--;
     } else if (styles.contains("increase")) {
+      // increase the count number if clicked on the increase button.
       count++;
     } else {
+      // if increse and decrease button not clicked show counter as 0.
       count = 0;
     }
     if (count > 0) {
+      // If the counter value is more than 0 change number to green.
       value.style.color = "green";
     }
     if (count < 0) {
+      // If the counter value is less than 0 change the number to red.
       value.style.color = "red";
     }
     if (count === 0) {
+      // If the counter value is equal to 0 than make the number black.
       value.style.color = "black";
     }
 
